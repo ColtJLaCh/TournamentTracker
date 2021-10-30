d70b67cb941beb67a0feeb58111c6a4587ac80e3
ColtJLaCh <CL51@myscc.ca>
2021-10-30 11:00:36 -0400

    Start modifying home and main for use

--- src/Main.java
+++ src/Main.java
@@ -13,10 +13,8 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         BorderPane root = new BorderPane();
-
-        Scene scene = new Scene(root,500,500);
-
-        stage.setTitle("Title Goes Here!");
+        Scene scene = new Scene(root,1024,768);
+        stage.setTitle("Tournament Tracker");
         stage.setScene(scene);
         stage.show();
     }
--- src/Pages/Home.java
+++ src/Pages/Home.java
@@ -1,4 +1,22 @@
 package Pages;
 
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.SceneAntialiasing;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Paint;
+
 public class Home {
+
+    Pane parentPane = new Pane();
+    Scene scene = new Scene(parentPane,1024,768);
+
+    public Home(Pane parentPane) {
+        this.parentPane = parentPane;
+    }
+
+    public Scene getScene() {
+        return this.scene;
+    }
 }
