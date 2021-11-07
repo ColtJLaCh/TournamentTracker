48d08c0631f82398d237715a1a77d00af9e34884
noahburrows <w0772238@myscc.ca>
2021-11-07 13:53:50 -0500

    #1: Create Database file

---
+++ src/main/java/Database/DBConst.java
@@ -0,0 +1,10 @@
+package Database;
+
+public class DBConst {
+
+    public static final String DB_NAME = "nburrowsfinal";
+    public static final String DB_USER = "nburrows";
+    public static final String DB_PASS = "ggq78ggq78dgz67dgz67";
+
+
+}
--- src/main/java/Database/Database.java
+++ src/main/java/Database/Database.java
@@ -1,4 +1,67 @@
 package Database;
+import java.sql.*;
 
 public class Database {
+    private static Database instance;
+    private Connection connection = null;
+
+    private Database(){
+        //Database connection
+        if(connection == null){
+            try{
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection =
+                        DriverManager.getConnection("jdbc:mysql://localhost/"
+                                        + DBConst.DB_NAME + "?serverTimezone=UTC",
+                                DBConst.DB_USER,
+                                DBConst.DB_PASS);
+                System.out.println("Created Connection");
+                //Create a basic table
+                //createTable(DBTableValues.TABLE_COIN,DBTableValues.CREATE_TABLE_COIN, connection);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static Database getInstance(){
+        if(instance == null){
+            instance = new Database();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void close(){
+        System.out.println("Closing connection");
+        try{
+            connection.close();
+        }
+        catch (Exception e){
+            connection = null;
+            e.printStackTrace();
+        }
+    }
+
+    private void createTable(String tableName, String tableQuery,
+                             Connection connection) throws SQLException {
+        Statement createTable;
+        //Get database information
+        DatabaseMetaData md = connection.getMetaData();
+        //Looking for the table with tableName
+        ResultSet resultSet = md.getTables("nburrowsfinal",
+                null, tableName, null);
+        //If the table is present
+        if(resultSet.next()){
+            System.out.println(tableName + " table already exists!");
+        }
+        else{
+            createTable = connection.createStatement();
+            createTable.execute(tableQuery);
+            System.out.println("The " + tableName + " table has been inserted");
+        }
+    }
 }
