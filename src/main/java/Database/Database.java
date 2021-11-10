97ec282ce4fa37c87481e024238f0c325659e1dc
noahburrows <w0772238@myscc.ca>
2021-11-10 14:06:40 -0500

    #4: Add command to insert into table

--- src/main/java/Database/Database.java
+++ src/main/java/Database/Database.java
@@ -23,6 +23,15 @@ public class Database {
 
                 createTable(basicTable, extras, connection);
 
+                //Add a player to table
+                String[] player = {"Chicago 25ers", "6", "4"};
+
+                //addToTable(basicTable, player, connection);
+
+                //Rertrieve table rows
+                getTable(basicTable, connection);
+
+                
                 //Drop the basic table
                 //dropTable(basicTable, connection);
             }catch (Exception e){
@@ -55,7 +64,7 @@ public class Database {
 
     private void createTable(String tableName, String[] extraColumns,
                              Connection connection) throws SQLException {
-        String sqlCreate = "CREATE TABLE " + tableName + " (" +
+        String sqlCreate = "CREATE TABLE `" + tableName + "` (" +
                 "ID" + " int NOT NULL AUTO_INCREMENT, " +
                 "Player" + " VARCHAR(50), ";
         for (int i = 0; i < extraColumns.length; i++) {
@@ -87,10 +96,47 @@ public class Database {
                 null, tableName, null);
         //If the table is present
         if(resultSet.next()){
-            //System.out.println("A tournament with the name " + tableName + " already exists!");
             Statement dropTable = connection.createStatement();
-            dropTable.execute("DROP TABLE " + tableName);
+            dropTable.execute("DROP TABLE `" + tableName + "`");
             System.out.println("Successfully deleted tournament " + tableName);
+        } else {
+            System.out.println("Error: Tournament doesn't exist.");
+        }
+    }
+
+    private void addToTable(String tableName, String[] newPlayer, Connection connection) throws SQLException {
+        String sqlInsert = "INSERT INTO `" + tableName + "` VALUES (0, '"+newPlayer[0] + "'";
+
+        for (int i = 1; i < newPlayer.length; i++) {
+            sqlInsert = sqlInsert  + ", '" + newPlayer[i]+ "'";
+        }
+        sqlInsert = sqlInsert + ");";
+
+        System.out.println(sqlInsert);
+        DatabaseMetaData md = connection.getMetaData();
+        //Looking for the table with tableName
+        ResultSet resultSet = md.getTables("nburrowsjava",
+                null, tableName, null);
+        //If the table is present
+        if(resultSet.next()){
+            Statement dropTable = connection.createStatement();
+            dropTable.execute(sqlInsert);
+            System.out.println("Added player to tournament " + tableName);
+        } else {
+            System.out.println("Error: Tournament doesn't exist.");
+        }
+    }
+
+    private void getTable(String tableName, Connection connection) throws SQLException {
+        DatabaseMetaData md = connection.getMetaData();
+        //Looking for the table with tableName
+        ResultSet resultSet = md.getTables("nburrowsjava",
+                null, tableName, null);
+        //If the table is present
+        if(resultSet.next()){
+            Statement dropTable = connection.createStatement();
+            dropTable.execute("SELECT * FROM `" + tableName + "`");
+
         } else {
             System.out.println("Error: Tournament doesn't exist.");
         }
