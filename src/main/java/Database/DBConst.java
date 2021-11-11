22932b70acd7f6953f39c861d6b8bfb4a0e32868
ColtJLaCh <CL51@myscc.ca>
2021-11-11 15:03:50 -0500

    Update .gitignore to not track db login info

--- src/main/java/Database/DBConst.java
+++ src/main/java/Database/DBConst.java
@@ -2,9 +2,9 @@ package Database;
 
 public class DBConst {
 
-    public static final String DB_NAME = "nburrowsjava";
-    public static final String DB_USER = "nburrows";
-    public static final String DB_PASS = "ggq78ggq78dgz67dgz67";
+    public static final String DB_NAME = "clachancejava";
+    public static final String DB_USER = "clachance";
+    public static final String DB_PASS = "4ssgp4ssgph7d2ch7d2c";
 
 
 }
