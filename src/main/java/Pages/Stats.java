accad65b365d993d97e24e74e55112e52d3ddc2d
ColtJLaCh <CL51@myscc.ca>
2021-11-20 12:34:21 -0500

    #6 Add dummy data to Stats page

--- src/main/java/Pages/Stats.java
+++ src/main/java/Pages/Stats.java
@@ -4,12 +4,21 @@ import javafx.geometry.Insets;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
+import java.util.ArrayList;
+
 /** PAGE CLASS
  * Constructor contains all layout information, add methods and properties as needed for functionality
  */
 public class Stats extends Page {
 
     //Anything with functionality goes here, Buttons, TextFields etc... as well as needed globals
+
+    //Data
+    String dataTourName = "";
+    String[] dataTeamArr; //The ordered array of teams to be matched up to players
+    String[] dataPlayerArr; //The ordered array of players to matched up to teams
+    String[] dataStats; //if teams stats 0 and 1 (Wins/Losses) will be applied to team, all others will be applied to players
+    String[][] dataStatsArr; // A 2d array for tracking the stats of each player
 
     public Stats() {
         //Initialize layout assets here, ImageViews, Panes, Text etc...
@@ -24,7 +33,27 @@ public class Stats extends Page {
     }
 
     //Local methods
+    private void dummyData() {
+        //Fake table data, this will be injected into these variables when functionality is programmed
 
+        //Singles
+        ///*
+        dataTourName = "DummyTournamentSINGLES";
+        dataStats = new String[] {"Wins", "Losses", "Time"};
+        dataPlayerArr = new String[]{"Jesse","Jamie","Bobby","Billy","Brody","Mort"};
+        dataTeamArr = new String[]{"NO TEAM","NO TEAM","NO TEAM","NO TEAM","NO TEAM","NO TEAM"};
+        dataStatsArr = new String[dataPlayerArr.length][dataStats.length];
+        //*/
+
+        //Teams
+        /*
+        dataTourName = "DummyTournamentTEAM";
+        dataStats = new String[] {"Wins", "Losses", "Time"};
+        dataPlayerArr = new String[]{"Jesse","Jamie","Bobby","Billy","Brody","Mort","Sherry","Donkey"};
+        dataTeamArr = new String[]{"TEAM1","TEAM2","TEAM3","TEAM4","TEAM5","TEAM6","TEAM7","TEAM8"};
+        dataStatsArr = new String[dataPlayerArr.length][dataStats.length];
+        */
+    }
 
     //Methods to add to pageBehavior
 
