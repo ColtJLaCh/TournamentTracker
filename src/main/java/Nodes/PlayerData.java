2f4cef4c1a689eef301c896826371f8e3fecc67b
ColtJLaCh <CL51@myscc.ca>
2021-11-20 12:27:36 -0500

    #6 Add layout Teams TableView to View page

--- src/main/java/Nodes/PlayerData.java
+++ src/main/java/Nodes/PlayerData.java
@@ -1,19 +1,19 @@
 package Nodes;
-
 public class PlayerData {
 
-    private String id;
-    private String name;
-    private String team;
-    private String[] stats;
+    private final String id;
+    private final String name;
+    private final String team;
 
-    public PlayerData(String id, String name, String team, String[] statsToAdd) {
+    private final String[] statVals;
+
+    public PlayerData(String id, String name, String team, String[] statsValsToAdd) {
         this.id = id;
         this.name = name;
         this.team = team;
-        this.stats = new String[statsToAdd.length];
-        for (int i = 0; i < stats.length; i++) {
-            this.stats[i] = statsToAdd[i];
+        this.statVals = new String[statsValsToAdd.length];
+        for (int i = 0; i < statsValsToAdd.length; i++) {
+            statVals[i] = statsValsToAdd[i];
         }
     }
 
@@ -21,46 +21,22 @@ public class PlayerData {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getTeam() {
         return team;
     }
 
-    public void setTeam(String team) {
-        this.team = team;
-    }
-
-    public String[] getStats() {
-        return stats;
-    }
-
-    public void setStats(String[] stats) {
-        this.stats = stats;
-    }
-
-    public String getStat(int index) {
-        return this.stats[index];
-    }
-
     public String[] getData() {
-        int dataLength = 3+this.stats.length;
+        int dataLength = 3+this.statVals.length;
         String[] dataString = new String[dataLength];
         dataString[0] = this.id;
         dataString[1] = this.name;
         dataString[2] = this.team;
-        for (int i = 0; i < this.stats.length; i++){
-            dataString[3+i] = this.stats[i];
+        for (int i = 0; i < this.statVals.length; i++){
+            dataString[3+i] = this.statVals[i];
         }
         return dataString;
     }
--- src/main/java/Pages/View.java
+++ src/main/java/Pages/View.java
@@ -2,6 +2,7 @@ package Pages;
 
 import HelpfulClasses.UsefulConstants;
 import Nodes.PlayerData;
+import javafx.beans.property.Property;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
@@ -10,6 +11,7 @@ import javafx.css.Style;
 import javafx.geometry.Insets;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.MapValueFactory;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
@@ -17,7 +19,7 @@ import javafx.scene.text.Text;
 import javafx.util.Callback;
 
 import java.lang.reflect.Array;
-import java.util.ArrayList;
+import java.util.*;
 
 /** PAGE CLASS
  * Constructor contains all layout information, add methods and properties as needed for functionality
@@ -44,62 +46,13 @@ public class View extends Page {
         //Initialize layout assets here, ImageViews, Panes, Text etc...
 
         dummyData(); //TODO: Replace this with retrieve method when functionality is added
+        insertData();
 
         Text tourTypeText = new Text("TYPE: " + (dataTourStyle == 0 ? "SINGLES TOURNAMENT" : "TEAMS TOURNAMENT"));
 
         classVBox = new VBox(tourTypeText); //Vbox needed for Top to Bottom layout, add assets here
 
-
-        int bracketNum = 0;
-        for (TableView bracket : brackets) {
-            ObservableList<String[]> bracketData = FXCollections.observableArrayList();;
-
-            bracket = new TableView();
-            bracket.setMaxWidth(UsefulConstants.DEFAULT_SCREEN_WIDTH/3);
-            bracket.setFixedCellSize(25);
-            bracket.setEditable(false);
-
-            for (int c = 0; c < columns.size(); c++) {
-                if (c == 0 || (dataTourStyle == 0 ? c == 1 : c == 2)/*To show either player or team name*/ || c == 3 || c == 4) {
-                    final int colNum = c;
-                    TableColumn tc = new TableColumn(columns.get(c));
-                    tc.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<String[], String>, ObservableValue<String>>() {
-                        @Override
-                        public ObservableValue<String> call(TableColumn.CellDataFeatures<String[], String> p) {
-                            return new SimpleStringProperty((p.getValue()[colNum]));
-                        }
-                    });
-                    bracket.getColumns().add(tc);
-                }
-            }
-
-            int amntPerBracket = 0;
-            if (dataTourStyle == 0) {
-                amntPerBracket = dataPlayerArr.length / brackets.length;
-            }else{
-                amntPerBracket = uniqueTeams.size() / brackets.length;
-            }
-
-            for (int br = bracketNum*amntPerBracket; br < bracketNum*amntPerBracket+amntPerBracket; br++) {
-                bracketData.add(playerData.get(br));
-            }
-
-            bracket.setItems(bracketData);
-
-            bracket.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-            bracket.setMinHeight(bracket.getFixedCellSize()*(bracket.getItems().size()+1));
-            bracket.setMaxHeight(bracket.getFixedCellSize()*(bracket.getItems().size()+1));
-            classVBox.getChildren().add(bracket);
-
-            Text setLine = new Text("-------------------------------------------------------------------------------------------------------------------------------------");
-
-            if (bracketNum % (int)Math.ceil(brackets.length/2) == 0 && dataSets > 1
-                    && bracketNum >= brackets.length/2 && bracketNum < brackets.length-1) {
-                classVBox.getChildren().add(setLine);
-            }
-
-            bracketNum++;
-        }
+        createBrackets();
 
         //classVBox.setAlignment(ALIGNMENT GOES HERE); //Usually Pos.TOP_LEFT
         classVBox.setPadding(new Insets(10,10,10,10)); //Set padding for Vbox (ORDER : double top, double right, double bottom, double left)
@@ -113,25 +66,27 @@ public class View extends Page {
         //Fake table data, this will be injected into these variables when functionality is programmed
 
         //Singles
+        ///*
         dataTourName = "DummyTournamentSINGLES";
         dataStats = new String[] {"Wins", "Losses", "Time"};
         dataPlayerArr = new String[]{"Jesse","Jamie","Bobby","Billy","Brody","Mort"};
         dataTeamArr = new String[]{"NO TEAM","NO TEAM","NO TEAM","NO TEAM","NO TEAM","NO TEAM"};
         dataStatsArr = new String[dataPlayerArr.length][dataStats.length];
         dataSets = 2;
-
+        //*/
 
         //Teams
         /*
         dataTourName = "DummyTournamentTEAM";
         dataStats = new String[] {"Wins", "Losses", "Time"};
-        dataPlayerArr = new String[]{"Jesse","Jamie","Bobby","Billy","Brody","Mort"};
-        dataTeamArr = new String[]{"TEAM1","TEAM1","TEAM1","TEAM2","TEAM2","TEAM2"};
+        dataPlayerArr = new String[]{"Jesse","Jamie","Bobby","Billy","Brody","Mort","Sherry","Donkey"};
+        dataTeamArr = new String[]{"TEAM1","TEAM2","TEAM3","TEAM4","TEAM5","TEAM6","TEAM7","TEAM8"};
         dataStatsArr = new String[dataPlayerArr.length][dataStats.length];
         dataSets = 2;
         */
+    }
 
-        //After table has been retrieved
+    private void insertData() {
         String lastTeam = "";
         for (String team : dataTeamArr) {
             if (team != "NO TEAM") dataTourStyle = 1;
@@ -142,7 +97,6 @@ public class View extends Page {
             }
         }
 
-        //TODO: Change this to pull stat values from table
         for (int p = 0; p < dataPlayerArr.length; p++) {
             for (int s = 0; s < dataStats.length; s++) {
                 dataStatsArr[p][s] = "0";
@@ -151,35 +105,97 @@ public class View extends Page {
 
         int amntOfSets = dataSets;
         if (dataTourStyle == 0) {
-            amntOfSets = (int)Math.ceil(dataPlayerArr.length/dataSets);
+            amntOfSets = (dataPlayerArr.length+dataSets-1)/dataSets;
         }else{
-            amntOfSets = (int)Math.ceil(dataTeamArr.length/dataSets);
+            if (!uniqueTeams.isEmpty())
+                amntOfSets = (uniqueTeams.size()+dataSets-1)/dataSets;
         }
 
         brackets = new TableView[amntOfSets];
-
 
         playerData = FXCollections.observableArrayList();
         for (int t = 0; t < uniqueTeams.size(); t++) {
             for (int p = 0; p < dataPlayerArr.length; p++) {
                 if (dataTourStyle == 0) {
                     String pId = String.valueOf(p);
-                    PlayerData dataToAdd = new PlayerData(pId, dataPlayerArr[p], "NO TEAM", dataStatsArr[p]);
-                    playerData.add(dataToAdd.getData());
+                    String[] dataToAdd = new PlayerData(pId, dataPlayerArr[p],"NO TEAM",dataStatsArr[p]).getData();
+                    playerData.add(dataToAdd);
                 }else{
                     String tId = String.valueOf(t);
-                    PlayerData dataToAdd = new PlayerData(tId, dataPlayerArr[p], uniqueTeams.get(t), dataStatsArr[p]);
-                    playerData.add(dataToAdd.getData());
+                    String[] dataToAdd = new PlayerData(tId, dataPlayerArr[p], uniqueTeams.get(t),dataStatsArr[p]).getData();
+                    playerData.add(dataToAdd);
                 }
             }
         }
+
         columns.add("Id");
         columns.add("Name");
         columns.add("Team");
         for (int cToAdd = 0; cToAdd < dataStats.length; cToAdd++) {
             columns.add(dataStats[cToAdd]);
         }
+    }
 
+    private void createBrackets() {
+        int bracketNum = 0;
+        int lastTeamInd = 0;
+        for (TableView bracket : brackets) {
+            ObservableList<String[]> bracketData = FXCollections.observableArrayList();
+            bracket = new TableView();
+
+            bracket.setMaxWidth(UsefulConstants.DEFAULT_SCREEN_WIDTH/3);
+            bracket.setFixedCellSize(25);
+            bracket.setEditable(false);
+
+            for (int c = 0; c < columns.size(); c++) {
+                if (c == 0 || (dataTourStyle == 0 ? c == 1 : c == 2) || c == 3 || c == 4) {
+                    TableColumn tc = new TableColumn();
+                    tc = new TableColumn(columns.get(c));
+                    final int index = c;
+                    tc.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<String[], String>, ObservableValue<String>>() {
+                        @Override
+                        public ObservableValue<String> call(TableColumn.CellDataFeatures<String[], String> p) {
+                            return new SimpleStringProperty((p.getValue()[index]));
+                        }
+                    });
+                    bracket.getColumns().add(tc);
+                }
+            }
+
+            if (dataTourStyle == 0) {
+                for (int br = bracketNum*dataSets; br < bracketNum*dataSets+dataSets; br++) {
+                    bracketData.add(playerData.get(br));
+                }
+            }else{
+                int br = 0;
+                for (int t = lastTeamInd; t < playerData.size(); t++) {
+                    if (playerData.get(t)[2] != playerData.get(lastTeamInd)[2] || t == 0) {
+                        bracketData.add(playerData.get(t));
+                        lastTeamInd = t;
+                        br++;
+                    }
+                    if (br >= dataSets) {
+                        break;
+                    }
+                }
+            }
+
+            bracket.setItems(bracketData);
+
+            bracket.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+            bracket.setMinHeight(bracket.getFixedCellSize()*(bracket.getItems().size()+1));
+            bracket.setMaxHeight(bracket.getFixedCellSize()*(bracket.getItems().size()+1));
+            classVBox.getChildren().add(bracket);
+
+            Text setLine = new Text("-------------------------------------------------------------------------------------------------------------------------------------");
+            if (brackets.length > 0) {
+                if (((bracketNum+1) % 2) == 0 && bracketNum+1 != brackets.length) {
+                    classVBox.getChildren().add(setLine);
+                }
+            }
+
+            bracketNum++;
+        }
     }
 
     //Methods to add to pageBehavior
