b3545119bd49c6dacfb94086b9b05947e143e39c
ColtJLaCh <CL51@myscc.ca>
2021-11-20 22:51:25 -0500

    Updated Delete page with some new methods

--- src/main/java/Pages/Delete.java
+++ src/main/java/Pages/Delete.java
@@ -21,8 +21,13 @@ public class Delete extends Page {
 
     //Initialze the database as global for use
     Database dbc = Database.getInstance();
+    Connection conn = dbc.getConnection();
 
     //Anything with functionality goes here, Buttons, TextFields etc... as well as needed globals
+    HBox hbox = new HBox();
+    ChoiceBox tourBox = new ChoiceBox();
+    VBox tablesVBox = new VBox(2);
+    Label tableLabel = new Label("Tournaments managed by " + DB_USER);
 
     Button deleteButton = new Button("END TOURNAMENT");
 
@@ -30,28 +35,19 @@ public class Delete extends Page {
         //Initialize layout assets here, ImageViews, Panes, Text etc...
         Label deletePageLabel = new Label("DELETE TOURNAMENTS");
 
+
+
         //First column for the Tables Choice Box
-        VBox tablesVBox = new VBox(2);
+
         tablesVBox.setPadding(new Insets(10,0,0,32));
-        Label tableLabel = new Label("Tournaments managed by " + DB_USER);
         tableLabel.setUnderline(true);
         tableLabel.setLabelFor(tablesVBox);
-        ChoiceBox tourBox = new ChoiceBox();
+
+
         tourBox.setMinWidth(200);
         tourBox.setMaxWidth(200);
-        Connection conn = dbc.getConnection();
-        //Gather all table names from the user's database
-        try {
-            DatabaseMetaData md = conn.getMetaData();
-            ResultSet rs = md.getTables(DB_NAME, null, "%", null);
-            //For each table, add its name to the choicebox
-            while (rs.next()) {
-                tourBox.getItems().add(rs.getString(3));
-            }
-        } catch (SQLException e) {
-            System.out.println("Error retrieving tables.");
-        }
-        tablesVBox.getChildren().addAll(tableLabel,tourBox);
+
+        reconstructChoiceBox();
 
         //Second column for the delete button
         VBox deleteVBox = new VBox(2);
@@ -73,8 +69,9 @@ public class Delete extends Page {
                     a.showAndWait();
                     if(a.getResult() == ButtonType.YES) {
                         if (dbc.dropTable(tourBox.getSelectionModel().getSelectedItem().toString(), conn)) {
+                            reconstructChoiceBox();
+                            reconstructClassVBox(deletePageLabel, hbox);
                             System.out.println("Tournament " + dropTour + " successfully deleted.");
-
                             //For Colton - Navigate to View page here!
                         } else {
                             System.out.println("Tournament doesn't exist.");
@@ -90,7 +87,7 @@ public class Delete extends Page {
         deleteVBox.setPadding(new Insets(10,0,0,0));
 
         //Add both columns to the HBox
-        HBox hbox = new HBox(tablesVBox, deleteVBox);
+        hbox = new HBox(tablesVBox, deleteVBox);
         hbox.setSpacing(64);
 
         classVBox = new VBox(deletePageLabel,hbox); //Vbox needed for Top to Bottom layout, add assets here
@@ -102,7 +99,22 @@ public class Delete extends Page {
     }
 
     //Local methods
-
+    public void reconstructChoiceBox() {
+        tourBox.getItems().clear();
+        tablesVBox.getChildren().clear();
+        //Gather all table names from the user's database
+        try {
+            DatabaseMetaData md = conn.getMetaData();
+            ResultSet rs = md.getTables(DB_NAME, null, "%", null);
+            //For each table, add its name to the choicebox
+            while (rs.next()) {
+                tourBox.getItems().add(rs.getString(3));
+            }
+        } catch (SQLException e) {
+            System.out.println("Error retrieving tables.");
+        }
+        tablesVBox.getChildren().addAll(tableLabel,tourBox);
+    }
 
     //Methods to add to pageBehavior
 
